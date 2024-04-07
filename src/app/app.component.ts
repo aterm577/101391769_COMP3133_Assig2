932d0d5dd328ec9ea2b8fd5dd98f634c7817ed82
aterm577 <aterm577@gmail.com>
2024-04-07 02:44:19 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = '101391769_comp3133_assig2';
+}
